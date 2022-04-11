b1506e86be17c30ba81ff102cfee74282d1e24cf
jxu477 <jxu477@gatech.edu>
2022-04-11 15:18:17 -0400

    home page

--- mlr_frontend/src/pages/HomePage/HomePage.js
+++ mlr_frontend/src/pages/HomePage/HomePage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import img from '../../assets/images/ml_image.png';
+
+import "./HomePage.css";
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="main">
+            <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" />
+                    <Nav className="nav-content">
+                        <Nav.Link className="nav-content-text" href="/">Log In</Nav.Link>
+                        <Nav.Link className="nav-content-text" href="/upload">Upload</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <div className="body">
+                <p>Machine Learning Roulette</p>
+            </div>
         </div>
     );
 };
