1264c4cebe2fd275d88cc707b440afc26591348f
jxu477 <jxu477@gatech.edu>
2022-08-27 23:11:18 -0400

    reformat python code

--- mlr_backend/run.py
+++ mlr_backend/run.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify, request
-from flask_jwt import JWT
-from flask_restful import Api
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,13 +6,14 @@ CORS(app)
 
 app.secret_key = "2130"
 
-## Process uploaded CSF File
+
 @app.route("/upload", methods=["POST"])
 def uploadCSVFile():
     request_data = request.get_json()
     print("request data is ")
     print(request_data)
     return jsonify({"message": "uploaded successfully"})
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
