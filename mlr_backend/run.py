8ecb91f290f41a222c5ae2169bb5bf0d40a9dea5
jxu477 <jxu477@gatech.edu>
2022-09-29 13:02:35 -0400

    complete kmeans api

--- mlr_backend/run.py
+++ mlr_backend/run.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
-import numpy as np
 from io import StringIO
+from flask_caching import Cache
+from kmeans.kmeans import kMeans
+import numpy as np
 
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "SimpleCache"
+cache = Cache(app)
 CORS(app)
 
 app.secret_key = "2130"
@@ -12,10 +16,7 @@ app.secret_key = "2130"
 @app.route("/upload", methods=["POST"])
 def uploadCSVFile():
     input_dataset = request.files['file'].read().decode('utf-8')
-    print(input_dataset)
-    # TODO: usecol param change
-    X_test = np.genfromtxt(StringIO(input_dataset),
-                           delimiter=',', usecols=np.arange(0, 5))
+    cache.set("dataset", input_dataset)
     return jsonify({"message": "uploaded successfully"})
 
 
@@ -24,7 +25,12 @@ def trainData():
     request_data = request.get_json()
     models = request_data.get('models')
     params = request_data.get('params')
-    # TODO: Use ML Models to train data and send back useful information
+    dataset = cache.get("dataset")
+    dataset_in_np_array = np.genfromtxt(StringIO(dataset),
+                                        delimiter=',', usecols=np.arange(0, 5))
+    if ("KMeans" in models):
+        centroid, clusterAssessment = kMeans(
+            dataset_in_np_array, int(params['Number_of_Clusters']))
     return jsonify({"message": "trained successfully"})
 
 
