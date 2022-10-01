ed4c3bef66b414403e8b97fd5c3095dbca43ad46
honeal3 <honeal3@gatech.edu>
2022-10-01 16:41:06 -0400

    Added Hierarchical Clustering and Upload Backend Framework

--- mlr_backend/heirCluster.py
+++ mlr_backend/heirCluster.py
@@ -2,8 +2,9 @@ from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 
 
-def heirClusting(data, seed=np.random(), clusters=2):
-    np.random.seed = seed
+def heirClusting(data, seed=None, clusters=2):
+    if seed is not None:
+        np.random.seed = seed
     clustering = AgglomerativeClustering(n_clusters=clusters).fit(data)
     # prediction = fit_predict(data)
     return clustering
