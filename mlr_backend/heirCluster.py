f6fe1a0782bb309b89f4a913b56b2ede953c01db
honeal3 <honeal3@gatech.edu>
2022-10-09 17:14:30 -0400

    Added documentation and prediction handling

--- mlr_backend/heirCluster.py
+++ mlr_backend/heirCluster.py
@@ -2,9 +2,15 @@ from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 
 
-def heirClusting(data, seed=None, clusters=2):
+# Generates a model based on training data
+def generateHeirClustingModel(data, seed=None, clusters=2):
     if seed is not None:
         np.random.seed = seed
     clustering = AgglomerativeClustering(n_clusters=clusters).fit(data)
     # prediction = fit_predict(data)
     return clustering
+
+# Uses the model to predict the placement of the input data
+def predictHeirClustering(model, data):
+    predictions = model.predict(data)
+    return predictions
