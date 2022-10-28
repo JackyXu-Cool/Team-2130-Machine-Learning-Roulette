4ae16adffffe4183308103bd00d7b1936f52e4e9
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2022-10-28 11:14:53 -0400

    completed split and tests

--- mlr_backend/split.py
+++ mlr_backend/split.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def splitDataset(X, ylabel, percent_to_split):
+def splitDataset(X, ylabel, percent_to_split=70):
     '''
     Splits the input dataset and y-label based on the percentage provided
     If a percentage is not provided, a default value of 70 is used instead.
@@ -12,15 +12,28 @@ def splitDataset(X, ylabel, percent_to_split):
     percentage = percent_to_split / 100
 
     Xrow,_ = X.shape
-    XrowIndex = np.ceil(Xrow * percentage)
+    XrowIndex = int(np.ceil(Xrow * percentage))
 
     Xtrain = X[:XrowIndex,:]
     Xtest = X[XrowIndex:,:]
 
-    Yrow,_ = ylabel.shape
-    YrowIndex = np.ceil(Yrow * percentage)
-    Ytrain = ylabel[:YrowIndex,:]
-    Ytest = ylabel[YrowIndex:,:]
+    Yrow = ylabel.shape[0]
+    YrowIndex = int(np.ceil(Yrow * percentage))
+    Ytrain = ylabel[:YrowIndex]
+    Ytest = ylabel[YrowIndex:]
 
 
     return Xtrain, Xtest, Ytrain, Ytest
+
+
+def testSplitDataset():
+    X = np.genfromtxt('testdata/X_test.csv', delimiter=',')
+    y = np.genfromtxt('testdata/y_test.csv', delimiter=',')
+    Xtrain1, Xtest1, ytrain1,ytest1 = splitDataset(X,y)
+
+    assert Xtrain1.shape[0]==74 and ytrain1.shape[0] == 74 and Xtrain1.shape[1] == X.shape[1], "The split dimensions is not correct"
+
+
+    Xtrain2, Xtest2, ytrain2,ytest2 = splitDataset(X,y,50)
+
+    assert Xtrain2.shape[0]==53 and ytrain2.shape[0] == 53 and Xtrain2.shape[1] == X.shape[1], "The split dimensions is not correct"
