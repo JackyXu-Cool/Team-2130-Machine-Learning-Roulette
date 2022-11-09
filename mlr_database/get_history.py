0ef20c6ede50f78a6c3dfd65c347afd9c0172a82
yliu3201 <yliu3201@gatech.edu>
2022-11-09 00:29:51 -0500

    Add some documentations

--- mlr_database/get_history.py
+++ mlr_database/get_history.py
@@ -3,6 +3,17 @@ import requests
 url = 'https://6ub4cm85uh.execute-api.us-east-1.amazonaws.com/trainingdata/gethistory'
 
 def get_training_result(email):
+    """
+    Retrieve user's training result' from the database using user's email address'
+
+    Args:
+        email (string): user's email address
+
+    Returns:
+        int: status code, 200 means success, other means there are errors
+        list: each entry will be a list of user's training results', please see readme for details.
+    """
+
     payload = {"email": f"{email}"}
 
     response = requests.post(url, json=payload)
